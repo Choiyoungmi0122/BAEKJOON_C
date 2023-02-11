1f1471308fd0989661bf86dc883058bae736bae9
Choiyoungmi <choiyoungmi2252@gmail.com>
2023-02-11 20:03:36 +0900

    while->for

--- 10773.c
+++ 10773.c
@@ -5,24 +5,24 @@
 //  Created by 최영미 on 2/11/23.
 //
 
-int sum, num[10000], count,i;
+int sum, num[10000],i;
 #include <stdio.h>
 int main(){
     int k;
     scanf("%d", &k);
     
-    while(count !=k){
-        scanf("%d", &num[i]);
+    for(int count =0;count<k;count++){
+        scanf("%d", &num[count]);
         
-        if(num[i]!=0)
-            sum = sum + num[i];
+        if(num[count]!=0)
+            sum = sum + num[count];
         else{
             sum = sum - num[i-1];
             i=i-2;
         }
-        count++;
-        i++;;
+        i++;
     }
+  
     
     printf("%d\n", sum);
 }
