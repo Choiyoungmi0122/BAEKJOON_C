2bcad3b120f4f3c5e3a777725925eec736607135
Choiyoungmi <choiyoungmi2252@gmail.com>
2023-02-27 15:02:39 +0900

    1157_clear

--- 1157.c
+++ 1157.c
@@ -6,19 +6,20 @@
 //
 
 #include <stdio.h>
-int fun(int nums[], int a,int max){
-    for(int i=0;i<26;i=i+2){
-        if(nums[i] != nums[i+1])
-            return nums[i];
+int fun(int al[], int max){
+    int count =0;
+    for(int i=0;i<26;i++){
+        if(al[i] == max)
+            count++;
     }
-    return 0;
+    return count;
 }
 int main(){
     char str[1000000]={0,};
-    int al[26]={0, },count =0,max=0,a=0;
-  
+    int al[26]={0, },max=0,a=0, result=0;
+    
     scanf("%s", str);
-
+    
     for(int i =0;str[i]!='\0';i++){
         if( str[i] < 91)     //대문자
             al[str[i] -65]++;
@@ -26,16 +27,19 @@ int main(){
             al[str[i]-97]++;
         }
     }
+    
     for(int i =0;i<26;i++){
-        printf("%d ", al[i]);
         if(max < al[i]){
-            a = i;
-            max = al[i];
+            a = i;  //al의 알파벳 위치
+            max = al[i];    //알파벳 개수
         }
     }
     
-    printf("\n");
-    printf("%c\n", a+65);
+    result = fun(al,max);
     
+    if(result == 1)
+        printf("%c\n", a+65);
+    else
+        printf("?\n");
     
 }
---
+++ 2346.c
@@ -0,0 +1,11 @@
+//
+//  2346.c
+//  BAEKJOON_C
+//
+//  Created by 최영미 on 2/27/23.
+//
+
+#include <stdio.h>
+int main(){
+    
+}
