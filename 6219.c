47cdd5f4ce089d8c21197abcb13faad9c8214214
Choiyoungmi <choiyoungmi2252@gmail.com>
2023-03-16 16:51:49 +0900

    6219_notclear

---
+++ 6219.c
@@ -0,0 +1,49 @@
+//
+//  6219.c
+//  BAEKJOON_C
+//
+//  Created by 최영미 on 3/16/23.
+//
+#include <stdio.h>
+
+int main(){
+    int A,B,D,count=0, a,b;
+    int num[1000]={0,},result[1000]={0, };
+    scanf("%d %d %d", &A, &B, &D);
+    
+    for(int i=A;i<=B;i++){
+        if(i==2){   //2는 따로 해야함
+            num[i]=i;       //2가 제일 작은 소수임
+        }
+        
+        for(int j=2;j<i;j++){
+            if(i%j==0)  //소수가 아니면 바로 멈춤
+                break;
+            else if(j==i-1){    //나누어 떨어지지 않으면서
+                num[i] = 1;
+            }
+        }
+    }
+    
+    
+    for(int i=A;i<=B;i++){
+        if(num[i]!=0){//소수인 경우에만 비교
+            if(i<10){
+                if(i == D)
+                    count++;
+            }
+            if(i>10){
+                a=i/10;
+                b=i%10;
+                if(a==D)
+                    count++;
+                if(b==D)
+                    count++;
+                
+            }
+        }
+    }
+    printf("%d\n", count);
+    return 0;
+    
+}
