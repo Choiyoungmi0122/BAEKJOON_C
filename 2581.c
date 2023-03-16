ccaa68bfcf76ee17977f0180d75af38bb862f765
Choiyoungmi <choiyoungmi2252@gmail.com>
2023-03-16 14:21:11 +0900

    2581_clear

--- 2581.c
+++ 2581.c
@@ -6,45 +6,38 @@
 //
 
 #include <stdio.h>
-
 int main(){
-    int n,m, sum=0, mm[10000]={0, },count=0;;
-    scanf("%d %d", &n, &m);
+    int M,N;
+    int min=0,sum=0;
     
-    int min = n>m?m:n;
-    int max = n<m?m:n;
+    scanf("%d %d", &N, &M);
     
-    for(int i=min;i<max;i++){
-        sum = sum + i;
-        for(int j=2;j<i;j++)
-        {
-            if(i%j ==0){
-                sum = sum - i;
-                break;
-            }
+    for(int i=N;i<=M;i++){
+        
+        if(i==2){   //2는 따로 해야함
+            sum = sum + i;
+            min = i;        //2가 제일 작은 소수임
         }
-    }
-    for(int i=min;i<=max;i++){
+        
         for(int j=2;j<i;j++){
-            if(i%j==0){ //소수아닌거 개수 + 어떤 숫자가 소수인지
-                mm[i]++;
+            
+            if(i%j==0)  //소수가 아니면 바로 멈춤
+                break;
+            
+            else if(j==i-1){    //나누어 떨어지지 않으면서
+                sum = sum + i;
+                if(min==0)  //처음 나오는 소수
+                    min=i;
             }
         }
     }
     
+    if(min!=0)  //소수가 안나오면 위에서 다 break 걸리기 때문에
+        printf("%d\n%d\n", sum, min);
+    else
+        printf("-1\n");
     
-    for(int i =min;i<=max;i++){
-        if(mm[i] != 0){
-            printf("%d ", i+1);
-            break;
-        }
-    }
-    
-    
-    printf("\n%d\n", sum);
-
 }
-
 //소수인가 아닌가를 먼저 판별해야함
 //소수 = 2 ~ (자기자신-1) 나누어떨어지지 않는 즉, != 0
 //소수이면 sum +, min>i 크기비교로 최소값 탐색
